738780a5b89b96b20f3a140c90fabef6dc12661e
KeerthanaN <natarajankeerthana96@gmail.com>
2026-04-06 21:48:19 +0530

    New code Kavin

--- src/test/java/org/test/ProjectClass.java
+++ src/test/java/org/test/ProjectClass.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 public class ProjectClass {
 	  @Test
 	  public void test() {
-		System.out.println("test");
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
 
 	}
 }
